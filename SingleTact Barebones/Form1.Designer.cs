fc98918daa6fa8fd0f35464f1f0b8420b4074d64
Dayi <9254911+dayizhang@users.noreply.github.com>
2021-05-27 17:56:04 +0100

    finish the barebone project

--- SingleTact Barebones/Form1.Designer.cs
+++ SingleTact Barebones/Form1.Designer.cs
@@ -28,46 +28,65 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.ForceOuptut = new System.Windows.Forms.Label();
+            this.AcquisitionWorker = new System.ComponentModel.BackgroundWorker();
+            this.guiTimer_ = new System.Windows.Forms.Timer(this.components);
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(59, 41);
+            this.label1.Location = new System.Drawing.Point(79, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.Size = new System.Drawing.Size(60, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Force = ";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(153, 41);
+            this.label2.Location = new System.Drawing.Point(204, 50);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.Size = new System.Drawing.Size(50, 17);
             this.label2.TabIndex = 0;
             this.label2.Text = "counts";
             // 
             // ForceOuptut
             // 
             this.ForceOuptut.AutoSize = true;
-            this.ForceOuptut.Location = new System.Drawing.Point(111, 41);
+            this.ForceOuptut.Location = new System.Drawing.Point(148, 50);
+            this.ForceOuptut.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.ForceOuptut.Name = "ForceOuptut";
-            this.ForceOuptut.Size = new System.Drawing.Size(21, 13);
+            this.ForceOuptut.Size = new System.Drawing.Size(24, 17);
             this.ForceOuptut.TabIndex = 0;
             this.ForceOuptut.Text = "##";
             // 
+            // AcquisitionWorker
+            // 
+            this.AcquisitionWorker.WorkerReportsProgress = true;
+            this.AcquisitionWorker.WorkerSupportsCancellation = true;
+            this.AcquisitionWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.AcquisitionWorker_DoWork);
+            this.AcquisitionWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.AcquisitionWorker_RunWorkerCompleted);
+            // 
+            // guiTimer_
+            // 
+            this.guiTimer_.Interval = 1;
+            this.guiTimer_.Tick += new System.EventHandler(this.guiTimer__Tick);
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(262, 123);
+            this.ClientSize = new System.Drawing.Size(349, 151);
             this.Controls.Add(this.ForceOuptut);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -80,6 +99,8 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label ForceOuptut;
+        private System.ComponentModel.BackgroundWorker AcquisitionWorker;
+        private System.Windows.Forms.Timer guiTimer_;
     }
 }
 
--- SingleTact Barebones/Form1.cs
+++ SingleTact Barebones/Form1.cs
@@ -1,20 +1,205 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Threading;
+using SingleTactLibrary;
 
 namespace SingleTact_Barebones
 {
     public partial class Form1 : Form
     {
+        private bool backgroundIsFinished_ = false;  // Flag to check background thread is finished
+        private double measuredFrequency_ = 50;  // Sensor update rate
+        private List<USBdevice> USBdevices = new List<USBdevice>();
+        private List<string> comPortList = new List<string>();
+        private SingleTact activeSingleTact;
+        private delegate void CloseMainFormDelegate(); //Used to close the program if hardware is not connected
         public Form1()
         {
+            string exceptionMessage = null;
+
             InitializeComponent();
+            var finder = new ComPortFinder();
+            // Get available serial ports.
+            comPortList = finder.findSingleTact();
+            if (comPortList.Count == 0)
+            {
+                MessageBox.Show(
+                "Failed to start sensor: no serial ports found.\n\nPlease connect the device then restart this application.",
+               "Hardware initialisation failed",
+               MessageBoxButtons.OK,
+               MessageBoxIcon.Exclamation);
+                // There's no point showing the GUI.  Force the app to auto-close.
+                Environment.Exit(-1);
+            }
+            else
+            {
+                for (int i = 0; i < 1; i++)
+                {
+                    USBdevice USB = new USBdevice();
+                    USB.Initialise(finder.prettyToComPort(comPortList[i]));
+                    USBdevices.Add(USB);
+                    this.Text = comPortList[i];
+                }
+            }
+            activeSingleTact = USBdevices[0].singleTact;
+            try
+            {
+                //PopulateGUIFields();
+                foreach (USBdevice USB in USBdevices)
+                {
+                    USB.singleTact.PushSettingsToHardware();
+                    RefreshFlashSettings_Click(this, null); //Get the settings from flash
+                }
+                //CreateStripChart();
+                AcquisitionWorker.RunWorkerAsync(); //Start the acquisition thread
+
+                guiTimer_.Start();
+            }
+            catch
+            {
+                string summary = "Failed to start sensor";
+
+                if (comPortList.Count == 0)
+                    summary += ": no serial ports detected.";
+                else
+                    summary += " on " + comPortList[0] + ".";
+
+                summary += "\n\n";
+
+                if (exceptionMessage != null)
+                    summary += exceptionMessage;
+                else
+                    summary += "Please connect the device then restart this application.";
+
+                MessageBox.Show(
+                    summary,
+                    "Hardware initialisation failed",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+
+                // There's no point showing the GUI.  Force the app to auto-close.
+                Environment.Exit(-1);
+            }
+        }
+        private void RefreshFlashSettings_Click(object sender, EventArgs e)
+        {
+            bool backgroundWasRunning = AcquisitionWorker.IsBusy;
+
+            if (backgroundWasRunning)
+                StopAcquisitionThread();
+
+            if (!activeSingleTact.PullSettingsFromHardware())
+            {
+                MessageBox.Show(
+                    "Failed to retrieve current settings.",
+                    "Error!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+            }
+
+            if (backgroundWasRunning)
+            {
+                StartAcquisitionThread();
+            }
+        }
+        /// <summary>
+        /// Stop acquisition thread
+        /// </summary>
+        private void StopAcquisitionThread()
+        {
+            AcquisitionWorker.CancelAsync();
+            while (false == backgroundIsFinished_)
+            {
+                System.Windows.Forms.Application.DoEvents(); //Wait for us to finish
+                Thread.Sleep(1);
+            }
+        }
+
+        /// <summary>
+        /// Start the Acquisition Thread
+        /// </summary>
+        private void StartAcquisitionThread()
+        {
+            AcquisitionWorker.RunWorkerAsync();
+        }
+        /// <summary>
+        /// Do work - process sensor data
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AcquisitionWorker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            backgroundIsFinished_ = false;
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            while (!worker.CancellationPending) //Do the work
+            {
+                foreach (USBdevice USB in USBdevices)
+                {
+                    SingleTact singleTact = USB.singleTact;
+                    SingleTactFrame newFrame = singleTact.ReadSensorData(); //Get sensor data
+
+                    if (null != newFrame) //If we have data
+                    {
+                        USB.addFrame(newFrame);             
+                    }
+                    else  // USB has been unplugged
+                    {
+                        new Thread(() =>
+                        {
+                            guiTimer_.Stop();
+                            backgroundIsFinished_ = true;
+                            var index = USBdevices.IndexOf(USB);
+                            var comPort = comPortList[index];
+                            var result = MessageBox.Show(
+                                comPort.ToString() + " has been unplugged.\nWould you like to save your data before exiting?",
+                                "Error!",
+                                MessageBoxButtons.YesNo,
+                                MessageBoxIcon.Error);
+                            Application.Exit();
+                        }).Start();
+                        backgroundIsFinished_ = false;
+                        StopAcquisitionThread();
+                        break;
+                    }
+
+                    //Calculate rate
+                    double delta = newFrame.TimeStamp - USB.lastTimeStamp; // calculate delta relative to previous sensor's last reading
+                    if (delta != 0)
+                        measuredFrequency_ = measuredFrequency_ * 0.95 + 0.05 * (1.0 / (delta));  //Averaging
+                                                                                                  //measuredFrequency_ = 1/delta;
+                    USB.setTimestamp(newFrame.TimeStamp);
+                }
+            }
+        }
+        /// <summary>
+        /// Done - we are closing down
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void AcquisitionWorker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            backgroundIsFinished_ = true;
+        }
+        private void guiTimer__Tick(object sender, EventArgs e)
+        {
+            foreach (USBdevice USB in USBdevices)
+            {
+                if (!backgroundIsFinished_)
+                    updateGraph(USB);
+            }
+        }
+
+        private void updateGraph(USBdevice USB)
+        {
+            if (USB.frameList.Count > 0)
+            {
+                var point = USB.frameList.Last();
+                ForceOuptut.Text = point.SensorData[0] + "";
+            }
         }
     }
 }
